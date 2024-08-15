d332fb33dd6fe921b1fd3c2a02a10c5b9ef99966
sabinatatarnikova-academic <sssaaabinatatarnikova@gmail.com>
2024-08-15 17:38:58 +0300

    Task 3.10

--- university-ui/src/main/java/com/foxminded/university/controller/DefaultController.java
+++ university-ui/src/main/java/com/foxminded/university/controller/DefaultController.java
@@ -1,12 +1,18 @@
 package com.foxminded.university.controller;
 
+import com.foxminded.university.model.dtos.response.CourseDTO;
+import com.foxminded.university.service.course.CourseService;
+import com.foxminded.university.utils.PageUtils;
+import com.foxminded.university.utils.RequestPage;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Locale;
 
@@ -14,8 +20,13 @@ import java.util.Locale;
 @AllArgsConstructor
 public class DefaultController {
 
+    private final CourseService courseService;
+
     @GetMapping("/")
-    public String getWelcomePage(Model model) {
+    public String getWelcomePage(Model model, @RequestParam(value = "page", defaultValue = "0") String pageStr, @RequestParam(value = "size", defaultValue = "10") String sizeStr) {
+        RequestPage page = PageUtils.createPage(pageStr, sizeStr);
+        Page<CourseDTO> coursesPage = courseService.findAllCoursesWithPagination(page);
+        model.addAttribute("courses", coursesPage);
         model.addAttribute("locale", Locale.ENGLISH);
         return "index";
     }
