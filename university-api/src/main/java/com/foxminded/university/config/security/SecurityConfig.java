21b8baf361dfd01aeb57a7dc0c01e64c897765ea
sabinatatarnikova-academic <sssaaabinatatarnikova@gmail.com>
2024-08-16 12:59:00 +0300

    Adding university api

---
+++ university-api/src/main/java/com/foxminded/university/config/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.foxminded.university.config.security;
+
+import com.foxminded.university.config.ServiceConfig;
+import com.foxminded.university.service.user.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Import(ServiceConfig.class)
+public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/admin").hasRole("ADMIN")
+                                .requestMatchers("/teacher").hasRole("TEACHER")
+                                .requestMatchers("/student").hasRole("STUDENT")
+                                .requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .userDetailsService(userDetailsService)
+                .formLogin((form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                        .successHandler(new CustomAuthenticationSuccessHandler())
+                        .failureUrl("/login?error=true")))
+                .csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/admin/schedule/classes/add")))
+                .csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/admin/schedule/edit")));
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web
+                .ignoring()
+                .requestMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/vendor/**", "/fonts/**");
+    }
+}
